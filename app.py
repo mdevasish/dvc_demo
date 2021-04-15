dc849b744417d492c07fb27214950c176ab3b946
mdevasish <mdevasish@gmail.com>
2021-04-15 19:37:42 +0800

    github workflows

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ webapp_root = "webapp"
 static_dir = os.path.join(webapp_root, "static")
 template_dir = os.path.join(webapp_root, "templates")
 
-app = Flask(__name__, static_folder=static_dir,template_folder=template_dir)
+app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -33,6 +33,7 @@ def index():
             return render_template("404.html", error=error)
     else:
         return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
